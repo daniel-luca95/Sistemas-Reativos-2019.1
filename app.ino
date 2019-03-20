796e4ae796af7a083c4ba4855140e21cd255b4c1
Daniel Luca Alves da Silva <danielluca@aluno.puc-rio.br>
2019-03-20 12:03:57 -0300

    Esboço inicial da tarefa

--- app.ino
+++ app.ino
@@ -1,22 +1,33 @@
 #include "event_driven.h"
 #include "app.h"
-//#include "pindefs.h"
 
-int LED =11;
+#define ON LOW
+#define OFF HIGH
 
-void appinit(void) 
+int button1 = A1;
+int button1State = HIGH;
+int button2 = A2;
+int button2State = HIGH;
+
+int LED = 10;
+int period = 1000;
+
+void appinit(void)
 {
   pinMode(LED, OUTPUT);
-  digitalWrite(LED, HIGH);
-  button_listen(A1);
+  digitalWrite(LED, OFF);
+  button_listen(button1);
+  button_listen(button2);
 }
 
-void button_changed(int p, int v) 
+void button_changed(int p, int v)
 {
-  digitalWrite(LED, v);
+  if(p==button1)
+	button1State = v;
+  else // is button 2
+	button2State = v;
+  if ( button1State == ON && buton2State == ON)
+    
 }
 
-void timer_expired(void) 
-{
-  // Empty
-}
+
