6fc741f464415b326617f0476b3f073a4483eace
Daniel Luca Alves da Silva <danielluca@tecgraf.puc-rio.br>
2019-05-17 19:22:01 -0300

    Abordagem mais genérica sendo tentada na main

--- LOVE/Mini Projeto 2/main.lua
+++ LOVE/Mini Projeto 2/main.lua
@@ -5,19 +5,29 @@ local DragonPackage = require "dragon"
 local TrollPackage = require "troll"
 --local menu = require "menu"
 
+enemies = {}
+
 function love.load()
   sceneManager.setScene(2) 
   math.randomseed(require "socket".gettime())
   hero = HeroPackage.newHero("herocorrected.png", 20, 200) --inicia herói com um sprite e posição inicial
-  troll = TrollPackage.newTroll("trollcorrected.png", 1100, 300)
-  heroine = HeroPackage.newHero("heroinecorrected.png", 850, 150)
-  mate = DragonPackage.newDragon("dragoncorrected.png", 600, 200)
   hero.setScene(sceneManager) --Seta a cena em que o heroi está no jogo
-  heroine.setScene(sceneManager)
-  mate.setScene(sceneManager)
-  troll.setScene(sceneManager)
---  menu.loadMenu()
-  
+    
+  initialPositions = {{395, 640}, {820,520}, {1100,440}}
+  for index, position in pairs(initialPositions) do
+    local enemy
+    enemy = TrollPackage.newTroll("trollcorrected.png", position[1], position[2])
+    enemies[enemy] = true
+    enemy.setDeathEvent( 
+      function () 
+        enemies[enemy] = nil
+      end
+    )
+    
+  end
+  for enemy, _ in pairs(enemies) do
+    enemy.setScene(sceneManager)
+  end
 end
 
 function love.keypressed(key) 
@@ -28,7 +38,11 @@ function love.keypressed(key)
   elseif key == "right" then
     hero.accelerate(180)
   elseif key == "space" then
-    hero.attack({mate, heroine, troll})
+    local enemyList = {}
+    for enemy, _ in pairs(enemies) do
+      table.insert(enemyList, enemy)
+    end    
+    hero.attack(enemyList)
   end
 end
 
@@ -44,28 +58,15 @@ end
 function love.draw()
   sceneManager.draw()
   hero.draw()
-  mate.draw()
-  heroine.draw()
-  troll.draw()
---  menu.draw()
+  for enemy, _ in pairs(enemies) do
+    enemy.draw()
+  end
 end
-
-index = 1
-vel = {-100, -100, -100, 400, 400, 400, -300, -300, -300}
 
 function love.update(dt)
   dt = math.min(dt, 0.1)
   hero.update(dt)
-  mate.update(dt)
-  heroine.update(dt)
-  troll.update(dt)
---  local action 
---  action = math.random(1, 15)
---  if action > 9 then
---    mate.accelerate(vel[index])
---    index = (index + 1) % #vel + 1
---  elseif action == 1 then
---    mate.jump()
---  end
-
+  for enemy, _ in pairs(enemies) do
+    enemy.update(dt)
+  end
 end
