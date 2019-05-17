df57a441ffdaaf4160646c1f6f19baef700d94b1
NOTEBOOK-BIANCA\biafr <biafrag@gmail.com>
2019-05-17 20:54:20 -0300

    Quase terminando menu principal

--- LOVE/Mini Projeto 2/main.lua
+++ LOVE/Mini Projeto 2/main.lua
@@ -3,7 +3,7 @@ local characterPackage = require "character"
 local HeroPackage = require "hero"
 local DragonPackage = require "dragon"
 local TrollPackage = require "troll"
---local menu = require "menu"
+local menu = require "menu"
 
 function love.load()
   sceneManager.setScene(1) 
@@ -17,48 +17,57 @@ function love.load()
   heroine.setScene(sceneManager)
   mate.setScene(sceneManager)
   troll.setScene(sceneManager)
---  menu.loadMenu()
+  menu.loadMenu()
   
 end
 
 function love.keypressed(key) 
-  if key == "up" then
-    hero.jump()
-  elseif key == "left" then
-    hero.accelerate(-180)
-  elseif key == "right" then
-    hero.accelerate(180)
-  elseif key == "space" then
-    hero.attack({mate, heroine, troll})
+  if(menu.start) then
+    if key == "up" then
+      hero.jump()
+    elseif key == "left" then
+      hero.accelerate(-180)
+    elseif key == "right" then
+      hero.accelerate(180)
+    elseif key == "space" then
+      hero.attack({mate, heroine, troll})
+    end
   end
 end
 
 function love.keyreleased(key)
-  if key == "left" then
-    hero.accelerate(180)
-  elseif key == "right" then
-    hero.accelerate(-180)
+  if menu.start then
+    if key == "left" then
+      hero.accelerate(180)
+    elseif key == "right" then
+      hero.accelerate(-180)
+    end
   end
 end
 
 
 function love.draw()
-  sceneManager.draw()
-  hero.draw()
-  mate.draw()
-  heroine.draw()
-  troll.draw()
---  menu.draw()
+  if(menu.start) then 
+    sceneManager.draw()
+    hero.draw()
+    mate.draw()
+    heroine.draw()
+    troll.draw()
+  else
+    menu.draw()
+  end
 end
 
 index = 1
 vel = {-100, -100, -100, 400, 400, 400, -300, -300, -300}
 
 function love.update(dt)
-  hero.update(dt)
-  mate.update(dt)
-  heroine.update(dt)
-  troll.update(dt)
+  if menu.start then
+    hero.update(dt)
+    mate.update(dt)
+    heroine.update(dt)
+    troll.update(dt)
+  end
 --  local action 
 --  action = math.random(1, 15)
 --  if action > 9 then
--- LOVE/Mini Projeto 2/menu.lua
+++ LOVE/Mini Projeto 2/menu.lua
@@ -1,14 +1,22 @@
 local Menu = {}
 
+-- Um botão tem um texto e uma função que é chamada quando ele é apertado 
 local function newButton(text, fn)
   return
   {
     text = text,
-    fn = fn
+    fn = fn,
+    
+    now = false,
+    last = false
   }
 end
-  local buttons = {}
-  local font = nil
+
+local buttons = {} --Tabela de botões do menu
+local font = nil -- Tamanho da fonte 
+Menu["start"] = false
+Menu["image"] = "Scene2.png"
+--Função que cria botões do Menu
   Menu["loadMenu"] =
   
   function()
@@ -16,7 +24,7 @@ end
     table.insert(buttons,newButton(
         "Start Game",
         function ()
-          print("Starting Game")
+          Menu["start"] = true
         end))
     
     table.insert(buttons,newButton(
@@ -25,35 +33,85 @@ end
           love.event.quit(0)
         end))
     end
-    
+  
+  --Função responsável por desenhar tudo relacionado ao menu
     Menu["draw"] =
     function()
-      local widthCanvas = love.graphics.getWidth()
-      local heightCanvas = love.graphics.getHeight()
+      love.graphics.setBackgroundColor(0.8,0.7,0.8,1) -- Cor de fundo
+      local widthCanvas = love.graphics.getWidth() -- largura da tela
+      local heightCanvas = love.graphics.getHeight() -- altura da tela
       
-      local button_width = widthCanvas * (1/3)
+      local button_width = widthCanvas * (1/3) -- largura do botão é 1/3 da largura da tela
       local button_height = 64
-      local total_height = button_height * #buttons
-      local margin = 16
+      local total_height = button_height * #buttons -- altura total dos botões juntos
+      local margin = 16 -- margem entre os botões
       local cursory = 0
-      for i, button in ipairs(buttons) do
-        love.graphics.setColor(0.4,0.4,0.5,1)
+      local bx = (widthCanvas * 0.5) - (button_width * 0.5) -- Posição inicial em x do botão meio da tela menos metade da largura do botão  
+      local by = (heightCanvas * 0.5) - (button_height * 0.5) - (total_height * 0.5) + cursory -- Posição inicial em y do botão considerando espaço entre botões
+      
+      love.graphics.setColor(0.7,0.3,0.5,0.8) -- Cor do retângulo em volta dos botões
+        --Retângulo em volta dos botões
         love.graphics.rectangle(
           "fill",
-          (widthCanvas * 0.5) - (button_width * 0.5), 
-          (heightCanvas * 0.5) - (button_width * 0.5) - (total_height * 0.5) * cursory,
-          button_width,
-          button_height 
+          bx - 20, 
+          by - 20,
+          button_width + 40, -- largura do botão
+          total_height + 40 + margin -- altura do botão
         )
-        love.graphics.setColor(0,0,0,1)
+      --Desenha todos os botões
+      for i, button in ipairs(buttons) do
+        by = (heightCanvas * 0.5) - (button_height * 0.5) - (total_height * 0.5) + cursory
+        
+        -- Atualiza estado do botão
+        button.last = button.now
+        local color = {0.8,0.5,0.6,1} -- Cor dos botões
+        local mx,my = love.mouse.getPosition() -- Pega posição do mouse
+        
+        -- Faz cálculo de interseção para ver se está com mouse em cima do botão ou não 
+        local hot = mx > bx  and mx < bx + button_width and 
+                    my > by  and my < by + button_height
+                    
+        -- Se o mouse estiver em cima do botão bota highlight nele
+        if hot then
+          color = {1,0.8,0.9,1}
+        end
+        
+        -- Checa se mouse tá apertando o botão
+        button.now = love.mouse.isDown(1)
+        
+        -- Se o mouse estiver pressionado, ele tiver mudado de estado e o mouse estiver em cima chama a função do botão
+        if button.now and not button.last and hot then
+          button.fn()
+        end 
+          
+        love.graphics.setColor(unpack(color)) -- unpack pega conteúdo da table e espalha nos argumentos da função  
+        
+        --Um botão é um retângulo preenchido
+        love.graphics.rectangle(
+          "fill",
+          bx, -- Posição inicial em x do botão meio da tela menos metade da largura do botão
+          by, -- Posição inicial em y do botão considerando espaço entre botões
+          button_width, -- largura do botão
+          button_height -- altura do botão
+        )
+        
+        local texW = font:getWidth(button.text) 
+        local texH = font:getHeight(button.text)
+        if not Menu.start then
+          love.graphics.setColor(0,0,0,1) -- Cor da letra 
+        else 
+          love.graphics.setColor(1,1,1,1) -- Cor da letra 
+        end
+        --Escrevendo texto do botão
         love.graphics.print(
           button.text,
           font,
-          (widthCanvas * 0.5) - (button_width * 0.5), 
-          (heightCanvas * 0.5) - (button_width * 0.5) - (total_height * 0.5) * cursory
+          (widthCanvas * 0.5) - (texW * 0.5), 
+          (heightCanvas * 0.5) - (button_height * 0.5) - (total_height * 0.5) + cursory + texH * 0.5
           )
-      cursory = cursory + (button_height * margin)
+      cursory = cursory + (button_height + margin) -- Fazendo cálculo para ter espaço vertical entre botões
       end
+
   end
   
       Menu["update"] =
